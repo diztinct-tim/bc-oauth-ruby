059b4c5f2c80d24fca171c5514b193553207d71b
Diztinct Tim <tim@diztinct.com>
2016-10-24 14:37:06 -0400

    trying to_json

--- hello.rb
+++ hello.rb
@@ -80,7 +80,7 @@ get '/' do
   @bc_api_url = bc_api_url
   @client_id = bc_client_id
   @products = JSON.pretty_generate(@store.bc_api.products)
-  @hooks = JSON.pretty_generate(@store.bc_api)
+  @hooks = @store.bc_api.to_json
 
   erb :index
 end
