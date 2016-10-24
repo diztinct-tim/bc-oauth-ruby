22d8db080dd2690d0c12d82a1a54f9e6f02f61ee
Diztinct Tim <tim@diztinct.com>
2016-10-24 14:56:18 -0400

    removed hooks

--- hello.rb
+++ hello.rb
@@ -80,7 +80,6 @@ get '/' do
   @bc_api_url = bc_api_url
   @client_id = bc_client_id
   @products = JSON.pretty_generate(@store.bc_api.products)
-  @hooks = JSON.pretty_generate(@store.bc_api.hooks)
 
   erb :index
 end
