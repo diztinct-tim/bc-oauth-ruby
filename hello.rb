99d3c00c61d63c745929fc71d23b1a5e4e1e772f
Diztinct Tim <tim@diztinct.com>
2016-10-24 14:38:43 -0400

    hooks

--- hello.rb
+++ hello.rb
@@ -80,7 +80,7 @@ get '/' do
   @bc_api_url = bc_api_url
   @client_id = bc_client_id
   @products = JSON.pretty_generate(@store.bc_api.products)
-  @hooks = @store.bc_api.to_json
+  @hooks = JSON.pretty_generate(@store.bc_api.webhooks)
 
   erb :index
 end
